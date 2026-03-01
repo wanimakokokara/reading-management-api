19e3847730d2a4b89915b6802151661c4b3d2c3c
wanimakokokara <155953660+wanimakokokara@users.noreply.github.com>
2026-03-01 06:36:04 +0000

    fix code

--- lib/reading-management-api-stack.ts
+++ lib/reading-management-api-stack.ts
@@ -1,36 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class ReadingManagementApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // 1. DynamoDBテーブルの作成 (読書データ保存用)
-    const bookTable = new dynamodb.Table(this, 'BookTable', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // ポートフォリオ用なのでスタック削除時にデータも消す設定
+    // 1. S3バケットの作成
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
-    // 2. Lambda関数の作成 (先ほど作ったPythonコードを指定)
-    const bookLambda = new lambda.Function(this, 'BookHandler', {
-      runtime: lambda.Runtime.PYTHON_3_12,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'app.handler',
-      environment: {
-        TABLE_NAME: bookTable.tableName, // 環境変数でテーブル名を渡す
+    // 2. CloudFrontディストリビューションの作成（最新のOAC設定）
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        // 修正ポイント：'new' を削除し、正しい関数を呼び出し
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
     });
 
-    // LambdaにDynamoDBへの読み書き権限を付与 (最小権限の原則)
-    bookTable.grantReadWriteData(bookLambda);
+    // 3. フロントエンドのデプロイ
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('./frontend/dist')],
+      destinationBucket: websiteBucket,
+      distribution: distribution,
+      distributionPaths: ['/*'],
+    });
 
-    // 3. API Gatewayの作成
-    new apigateway.LambdaRestApi(this, 'BookApi', {
-      handler: bookLambda,
+    // 4. URLの出力
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'The URL of the deployed website',
     });
   }
 }
